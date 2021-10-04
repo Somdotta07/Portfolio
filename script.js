3a7c4fe3083e616dd4e526deaf577e2bde194f92
Somdotta07 <jhilick.s@gmail.com>
2021-10-04 17:10:58 +0200

    Add Mobile-menu with js
    --Add linters for js
    --Add script.js file
    --Add some js code
    --Add html div for mobile menu

---
+++ script.js
@@ -0,0 +1,18 @@
+let hamburger = document.querySelector('.menu');
+let toolbar =document.getElementById('mobile-menu');
+
+hamburger.addEventListener('click',()=>{
+    toolbar.classList.remove('display-none');
+});
+
+let closeMenu =document.getElementById('close-menu');
+closeMenu.addEventListener('click', ()=>{
+    toolbar.classList.add('display-none');
+});
+let links = document.querySelectorAll('.menu-links');
+
+for (let i=0; i<links.length ; i++){
+    links[i].addEventListener('click',()=>{
+        toolbar.classList.add('display-none');
+    });
+}
