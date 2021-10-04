a146927d53b3fb051014ad719fe1dc0ec7268ecb
Somdotta07 <jhilick.s@gmail.com>
2021-10-05 01:41:16 +0200

    Mobile-menu with js
    --Add nav for mobile menu
    --Adjust styling
    --Add js code for responsiveness
    --Fixed linter errors

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-let hamburger = document.querySelector('.menu');
-let toolbar =document.getElementById('.mobile-menu');
-
-hamburger.addEventListener('click',()=>{
-    toolbar.classList.remove('display-none');
+/* eslint-disable linebreak-style */
+const hamburger = document.querySelector('.hamburger');
+const toolbar = document.querySelector('.mobile-menu');
+hamburger.addEventListener('click', () => {
+  toolbar.classList.remove('display-mobile');
 });
-
-let closeMenu =document.getElementById('close-menu');
-closeMenu.addEventListener('click', ()=>{
-    toolbar.classList.add('display-none');
+const closeMenu = document.querySelector('header .mobile-menu .close-menu a');
+closeMenu.addEventListener('click', () => {
+  toolbar.classList.add('display-mobile');
 });
-let links = document.querySelectorAll('.menu-links');
-
-for (let i=0; i<links.length ; i++){
-    links[i].addEventListener('click',()=>{
-        toolbar.classList.add('mobile-menu');
-    });
+const links = document.querySelectorAll('header .mobile-menu ul li a');
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener('click', () => {
+    toolbar.classList.add('display-mobile');
+  });
 }
