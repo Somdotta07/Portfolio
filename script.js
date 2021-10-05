d5033a90cfee412f928266300e1509ad53843444
Somdotta07 <jhilick.s@gmail.com>
2021-10-05 14:27:20 +0200

    Added JS code
    -- Added modal,backdrop and basic structure for popup window

--- script.js
+++ script.js
@@ -14,3 +14,61 @@ for (let i = 0; i < links.length; i += 1) {
     toolbar.classList.add('display-mobile');
   });
 }
+
+const details = {
+  name: '',
+  description: '',
+  image: '',
+  technologies: [],
+  link:'',
+  source: '',
+}
+
+
+const openButton = document.getElementById('open');
+const projectContainer = document.querySelector('.project-container');
+let backdrop;
+let modal;
+function closeModal() {
+  if (backdrop) {
+    backdrop.remove();
+  }
+}
+
+openButton.addEventListener('click', () => {
+  // backdrop
+  backdrop = document.createElement('div');
+  backdrop.classList.add('backdrop');
+  backdrop.addEventListener('click', closeModal);
+  document.body.insertBefore(backdrop, projectContainer);
+// Modal
+
+  modal = document.createElement('div');
+  modal.classList.add('modal');
+  //Heading
+  const heading = document.createElement('h3');
+  heading.textContent = 'Multi Post Stories';
+  modal.appendChild(heading);
+  //Technologies
+  const technologies = document.createElement('ul');
+  technologies.appendChild(modal);
+ for (let key in Object){
+   let li =document.createElement('li');
+   li.textContent= this.technologies;
+   li.appendChild(technologies)
+ }
+  //paragraph
+  const description = document.createElement('p');
+  description.textContent(details.description);
+  description.classList.add('modal-description');
+  modal.appendChild(description);
+  //image
+  const image = document.createElement('image');
+  image.classList.add('modal-image');
+  modal.appendChild(image);
+  //Buttons
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button-actions');
+  modal.appendChild(buttonContainer);
+  
+})
