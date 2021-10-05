8219c7c207e8510ceb274492b02188ebce7ed092
Somdotta07 <jhilick.s@gmail.com>
2021-10-06 01:29:21 +0200

    Make changes in js and CSS
    -Add js codes
    - Add styles to css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-loop-func */
 /* eslint-disable linebreak-style */
 const hamburger = document.querySelector('.hamburger');
 const toolbar = document.querySelector('.mobile-menu');
@@ -15,20 +17,52 @@ for (let i = 0; i < links.length; i += 1) {
   });
 }
 
-const details = {
-  name: '',
-  description: '',
-  image: '',
-  technologies: [],
-  link:'',
-  source: '',
-}
+// --POP-UP
 
+const projectCard = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'media/image.png',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
+    link: '#',
+    source: '#',
+  },
 
+  {
+    name: 'Multi Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'media/layout2nd.png',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
+    link: '#',
+    source: '#',
+  },
+
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'media/layout2nd.png',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
+    link: '#',
+    source: '#',
+  },
+
+  {
+    name: 'Multi Post Stories ',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'media/layout2nd.png',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
+    link: '#',
+    source: '#',
+  },
+];
+//  ---Object declaration----//
 const openButton = document.getElementById('open');
 const container = document.querySelector('.container');
 let backdrop;
-let modal;
+// let modal;
+
+//  --For loop----
 function closeModal() {
   if (backdrop) {
     backdrop.remove();
@@ -36,60 +70,62 @@ function closeModal() {
 }
 
 openButton.addEventListener('click', () => {
-  // backdrop
   backdrop = document.createElement('div');
   backdrop.classList.add('backdrop');
   backdrop.addEventListener('click', closeModal);
   document.body.insertBefore(backdrop, container);
-// Modal
+  // Modal
 
-  modal = document.createElement('div');
+  const modal = document.createElement('div');
   modal.classList.add('modal');
-  //Heading
-  const heading = document.createElement('h3');
-  heading.textContent = 'Multi Post Stories';
+  // Heading
+  const heading = document.createElement('h2');
+  heading.textContent = projectCard[0].name;
   modal.appendChild(heading);
-  //Technologies
+  // Technologies
   const technologies = document.createElement('ul');
   technologies.classList.add('technologies-container');
   modal.appendChild(technologies);
 
   const techList1 = document.createElement('li');
-  techList1.textContent = 'HTML';
+  techList1.textContent = projectCard[0].technologies[0];
   techList1.classList.add('techlist');
   technologies.appendChild(techList1);
 
   const techList2 = document.createElement('li');
-  techList2.textContent = 'CSS';
+  techList2.textContent = projectCard[0].technologies[1];
   techList2.classList.add('techlist');
   technologies.appendChild(techList2);
 
   const techList3 = document.createElement('li');
-  techList3.textContent = 'JAVASCRIPT';
+  techList3.textContent = projectCard[0].technologies[2];
   techList3.classList.add('techlist');
   technologies.appendChild(techList3);
 
-  //paragraph
+  // paragraph
   const description = document.createElement('p');
-  description.textContent = 'Lorem Ipsum';
+  description.textContent = projectCard[0].description;
   description.classList.add('modal-description');
   modal.appendChild(description);
-  //image
-  const image = document.createElement('image');
+  // image
+  const image = document.createElement('img');
   image.classList.add('modal-image');
+  image.src = projectCard[0].image;
   modal.appendChild(image);
-  //Buttons
+  // Buttons
   const buttonContainer = document.createElement('div');
   buttonContainer.classList.add('button-actions');
   modal.appendChild(buttonContainer);
-    //Button 1
+  // Button 1
   const button1 = document.createElement('button');
   button1.setAttribute('type', 'button');
   button1.classList.add('buttonLink');
+  button1.href = projectCard[0].link;
   buttonContainer.appendChild(button1);
 
   const button2 = document.createElement('button');
   button2.setAttribute('type', 'button');
+  button2.href = projectCard[0].source;
   button2.classList.add('buttonSource');
   buttonContainer.appendChild(button2);
 
