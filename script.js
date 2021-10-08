9e0ecbb10a45e2765abca99942ed63bc572fcbd1
Somdotta07 <jhilick.s@gmail.com>
2021-10-08 20:14:50 +0200

    Pop-up final
    --Adjust design for desktop and mobile
    --Remove bugs

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ const inputName = document.getElementById('name');
 const inputEmail = document.getElementById('email');
 const inputMsg = document.getElementById('msg');
 
+const projectCard = [
+  {
+    name: 'Tonic',
+    image: 'media/pop-up-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
+    link: '#',
+    source: '#',
+  },
+];
+
 // Mobile menu
 hamburger.addEventListener('click', () => {
   toolbar.classList.remove('display-mobile');
@@ -25,123 +36,168 @@ for (let i = 0; i < links.length; i += 1) {
   });
 }
 
-const projectCard = [
-  {
-    name: 'Tonic',
-    image: 'media/image.png',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
-    link: '#',
-    source: '#',
-  },
-
-  {
-    name: 'Multi Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: 'media/layout2nd.png',
-    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
-    link: '#',
-    source: '#',
-  },
-
-  {
-    name: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: 'media/layout2nd.png',
-    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
-    link: '#',
-    source: '#',
-  },
-
-  {
-    name: 'Multi Post Stories ',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: 'media/layout2nd.png',
-    technologies: ['HTML', 'CSS', 'JAVASCRIPT'],
-    link: '#',
-    source: '#',
-  },
-];
-
 //  ---Object declaration----//
+
 const openButton = document.querySelectorAll('.modal-btn');
 const container = document.querySelector('.container');
-let backdrop;
+const backdrop = document.createElement('div');
+const modal = document.createElement('div');
+
+function addElement() {
+  backdrop.classList.add('backdrop');
+  document.body.insertBefore(backdrop, container);
+  // Modal
+  modal.classList.add('modal');
+  const headingContainer = document.createElement('div');
+  headingContainer.classList.add('headingContainer');
+  modal.appendChild(headingContainer);
+  // Heading
+  const heading = document.createElement('h2');
+  heading.textContent = projectCard[0].name;
+  headingContainer.appendChild(heading);
+  // Technologies
+  const technologies = document.createElement('ul');
+  technologies.classList.add('technologies-container');
+  modal.appendChild(technologies);
+  // Tech groups
+  const techList1 = document.createElement('li');
+  techList1.textContent = projectCard[0].technologies[0];
+  techList1.classList.add('techlist');
+  technologies.appendChild(techList1);
+  const techList2 = document.createElement('li');
+  techList2.textContent = projectCard[0].technologies[1];
+  techList2.classList.add('techlist');
+  technologies.appendChild(techList2);
+  const techList3 = document.createElement('li');
+  techList3.textContent = projectCard[0].technologies[2];
+  techList3.classList.add('techlist');
+  technologies.appendChild(techList3);
+  // Img container
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('image-container');
+  modal.appendChild(imageContainer);
+  // image
+  const image = document.createElement('img');
+  image.classList.add('modal-image');
+  image.src = projectCard[0].image;
+  imageContainer.appendChild(image);
+  // paragraph
+  const description = document.createElement('p');
+  description.textContent = projectCard[0].description;
+  description.classList.add('modal-description');
+  imageContainer.appendChild(description);
+  // Buttons
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button-actions');
+  modal.appendChild(buttonContainer);
+  // Button 1
+  const button1 = document.createElement('button');
+  button1.setAttribute('type', 'button');
+  button1.classList.add('buttonLink');
+  button1.innerHTML = 'See Live <img src= "media/Icon.png"> ';
+  button1.href = projectCard[0].link;
+  buttonContainer.appendChild(button1);
+  // Button 2
+  const button2 = document.createElement('button');
+  button2.setAttribute('type', 'button');
+  button2.classList.add('buttonSource');
+  button2.href = projectCard[0].source;
+  button2.innerHTML = 'See Source <i class="fab fa-github"></i>';
+  buttonContainer.appendChild(button2);
+  document.body.insertBefore(modal, container);
+
+  // Cross Button
+  const closeButton = document.createElement('img');
+  closeButton.classList.add('closeBtnImg');
+  closeButton.src = 'media/crossBtn.png';
+  headingContainer.appendChild(closeButton);
+  closeButton.addEventListener('click', () => {
+    modal.remove();
+    backdrop.remove();
+  });
+}
 
 openButton.forEach((event) => {
   event.addEventListener('click', () => {
-    backdrop = document.createElement('div');
-    backdrop.classList.add('backdrop');
-    document.body.insertBefore(backdrop, container);
-    // Modal
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
-    // Heading
-    const heading = document.createElement('h2');
-    heading.textContent = projectCard[0].name;
-    modal.appendChild(heading);
-    // Technologies
-    const technologies = document.createElement('ul');
-    technologies.classList.add('technologies-container');
-    modal.appendChild(technologies);
-    // Tech groups
-    const techList1 = document.createElement('li');
-    techList1.textContent = projectCard[0].technologies[0];
-    techList1.classList.add('techlist');
-    technologies.appendChild(techList1);
-    const techList2 = document.createElement('li');
-    techList2.textContent = projectCard[0].technologies[1];
-    techList2.classList.add('techlist');
-    technologies.appendChild(techList2);
-    const techList3 = document.createElement('li');
-    techList3.textContent = projectCard[0].technologies[2];
-    techList3.classList.add('techlist');
-    technologies.appendChild(techList3);
-    // image
-    const image = document.createElement('img');
-    image.classList.add('modal-image');
-    image.src = projectCard[0].image;
-    modal.appendChild(image);
-    // paragraph
-    const description = document.createElement('p');
-    description.textContent = projectCard[0].description;
-    description.classList.add('modal-description');
-    modal.appendChild(description);
-    // Buttons
-    const buttonContainer = document.createElement('div');
-    buttonContainer.classList.add('button-actions');
-    modal.appendChild(buttonContainer);
-    // Button 1
-    const button1 = document.createElement('button');
-    button1.setAttribute('type', 'button');
-    button1.classList.add('buttonLink');
-    button1.textContent = 'See Live';
-    button1.href = projectCard[0].link;
-    buttonContainer.appendChild(button1);
-    // Button 2
-    const button2 = document.createElement('button');
-    button2.setAttribute('type', 'button');
-    button2.classList.add('buttonSource');
-    button2.href = projectCard[0].source;
-    button2.textContent = 'See Source';
-    buttonContainer.appendChild(button2);
-    document.body.insertBefore(modal, container);
-
-    // Cross Button
-    const closeButton = document.createElement('img');
-    closeButton.classList.add('closeBtnImg');
-    closeButton.src = 'media/crossBtn.png';
-    modal.appendChild(closeButton);
-    closeButton.addEventListener('click', () => {
-      modal.remove();
-      backdrop.remove();
-    });
+    addElement();
   });
 });
 
+// window.addEventListener('scroll', () => {
+//   const scroll = window.pageYOffset;
+//   if (scroll > 50) {
+//     container.classList.remove('active');
+//   }
+// });
+
 // openButton.forEach((event) => {
 //   event.addEventListener('click', () => {
-//     addElement();
+//     backdrop = document.createElement('div');
+//     backdrop.classList.add('backdrop');
+//     document.body.insertBefore(backdrop, container);
+//     // Modal
+//     const modal = document.createElement('div');
+//     modal.classList.add('modal');
+//     // Heading
+//     const heading = document.createElement('h2');
+//     heading.textContent = projectCard[0].name;
+//     modal.appendChild(heading);
+//     // Technologies
+//     const technologies = document.createElement('ul');
+//     technologies.classList.add('technologies-container');
+//     modal.appendChild(technologies);
+//     // Tech groups
+//     const techList1 = document.createElement('li');
+//     techList1.textContent = projectCard[0].technologies[0];
+//     techList1.classList.add('techlist');
+//     technologies.appendChild(techList1);
+//     const techList2 = document.createElement('li');
+//     techList2.textContent = projectCard[0].technologies[1];
+//     techList2.classList.add('techlist');
+//     technologies.appendChild(techList2);
+//     const techList3 = document.createElement('li');
+//     techList3.textContent = projectCard[0].technologies[2];
+//     techList3.classList.add('techlist');
+//     technologies.appendChild(techList3);
+//     // image
+//     const image = document.createElement('img');
+//     image.classList.add('modal-image');
+//     image.src = projectCard[0].image;
+//     modal.appendChild(image);
+//     // paragraph
+//     const description = document.createElement('p');
+//     description.textContent = projectCard[0].description;
+//     description.classList.add('modal-description');
+//     modal.appendChild(description);
+//     // Buttons
+//     const buttonContainer = document.createElement('div');
+//     buttonContainer.classList.add('button-actions');
+//     modal.appendChild(buttonContainer);
+//     // Button 1
+//     const button1 = document.createElement('button');
+//     button1.setAttribute('type', 'button');
+//     button1.classList.add('buttonLink');
+//     button1.textContent = 'See Live';
+//     button1.href = projectCard[0].link;
+//     buttonContainer.appendChild(button1);
+//     // Button 2
+//     const button2 = document.createElement('button');
+//     button2.setAttribute('type', 'button');
+//     button2.classList.add('buttonSource');
+//     button2.href = projectCard[0].source;
+//     button2.textContent = 'See Source';
+//     buttonContainer.appendChild(button2);
+//     document.body.insertBefore(modal, container);
+
+//     // Cross Button
+//     const closeButton = document.createElement('img');
+//     closeButton.classList.add('closeBtnImg');
+//     closeButton.src = 'media/crossBtn.png';
+//     modal.appendChild(closeButton);
+//     closeButton.addEventListener('click', () => {
+//       modal.remove();
+//       backdrop.remove();
+//     });
 //   });
 // });
 
